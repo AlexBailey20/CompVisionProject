fafa02cbeca78d524f257ae5a76cacfad0949cdd
Shishir Tandale <sct2uq@virginia.edu>
2017-05-02 23:12:56 -0400

    fixed GAN divergence problems, simplified logic where possible, added more tunable parameters to GAN model

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from PIL import Image
 
 from models.generativeAdversarial import GAN_ISR_Model
 
-def get_inputs(size_x=(512,512), size_y=(128,128), limit=None):
-    file_path = '../data/faces/*.jpg'
+def get_inputs(size_x=(256,256), size_y=(64,64), limit=None):
+    file_path = '../data/flowers/*.jpg'
     files = glob.glob(file_path) if limit is None else glob.glob(file_path)[:limit]
     def channels_last_data_format(img):
         return np.swapaxes(np.swapaxes(img, 1, 2), 0, 1)
@@ -17,5 +17,5 @@ def get_inputs(size_x=(512,512), size_y=(128,128), limit=None):
     return zip(*map(process_img_file, files))
 
 orig, small = get_inputs()
-model = GAN_ISR_Model(orig, small, batch_size=10)
+model = GAN_ISR_Model(orig)
 model.train()
